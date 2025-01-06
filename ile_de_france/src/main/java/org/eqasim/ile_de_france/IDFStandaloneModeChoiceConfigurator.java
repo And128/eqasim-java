dfde118655143a8560b6170fb3261d4f7d6f0468
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-01-06 13:24:19 +0100

    consider in standalone mode choice

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFStandaloneModeChoiceConfigurator.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/IDFStandaloneModeChoiceConfigurator.java
@@ -2,6 +2,7 @@ package org.eqasim.ile_de_france;
 
 import org.eqasim.core.standalone_mode_choice.StandaloneModeChoiceConfigurator;
 import org.eqasim.ile_de_france.mode_choice.IDFModeChoiceModule;
+import org.eqasim.ile_de_france.policies.PolicyExtension;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.AbstractModule;
@@ -9,11 +10,13 @@ import org.matsim.core.controler.AbstractModule;
 import java.util.List;
 
 public class IDFStandaloneModeChoiceConfigurator extends StandaloneModeChoiceConfigurator {
+	private final PolicyExtension policies = new PolicyExtension();
+	
     public IDFStandaloneModeChoiceConfigurator(Config config, CommandLine commandLine) {
         super(config, commandLine);
     }
-
+    
     protected List<AbstractModule> getSpecificModeChoiceModules() {
-        return List.of(new IDFModeChoiceModule(this.getCommandLine()));
+        return List.of(new IDFModeChoiceModule(this.getCommandLine()), policies);
     }
 }
